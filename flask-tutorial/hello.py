37e37f5237924cc70655e3e1e5a7e308be6edf7a
robin-msk <robin.musyoki@kibo.school>
2023-02-14 13:45:31 +0300

    learning URL Building

--- flask-tutorial/hello.py
+++ flask-tutorial/hello.py
@@ -1,8 +1,33 @@
 from flask import Flask
+from flask import url_for
 
 app = Flask(__name__)
 
 # @app.route("/")
 
-def hello_world():
-    return "<p>Hello, World!</p>"
+# def hello_world():
+#     return "<h1>Am msk_robin, Learning flask is fun!</h1>"
+
+
+@app.route("/projects/")
+def projects():
+    return "The project page"
+
+@app.route('/about')
+def about():
+    return 'The about page'    
+
+
+# learning URL Building  { url_for()  }
+@app.route('/')
+def index():
+    return 'index'   
+
+@app.route('/login')
+def login():
+    return 'login'
+
+@app.route('/user/<username>')
+def profile (username):
+    username = "kendy"
+    return f'{username}\'s profile'
